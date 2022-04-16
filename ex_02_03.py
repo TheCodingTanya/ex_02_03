886f91cd570cce98c0ccc24f73d4dce90e98ed3b
Tanya <thetwtanya@gmail.com>
2022-04-16 13:27:55 +0800

    add coursera version

--- ex_02_03.py
+++ ex_02_03.py
@@ -3,7 +3,7 @@ xr = input("Enter Rate: ")
 xp = float(xh) * float(xr)
 print("Pay:",xp)
 
-
+# ex_03_01.py
 hrs = input("Enter Hours:")
 h = float(hrs)
 rate = input("Enter Rate:")
@@ -15,3 +15,20 @@ else:
     gp = float(40 * r + (h-40) * (1.5 * r))
 
 print(gp)
+
+# ex_03_01.py_coursera
+sh = input("Enter Hours: ")
+sr = input("Enter Rate: ")
+try:
+    fh = float(sh)
+    fr = float(sr)
+except:
+    print("Error, please enter numeric input")
+# print(fh, fr)
+if fh > 40:
+    reg = fr * fh
+    otp = (fh - 40.0) * (fr * 0.5)
+    xp = reg + otp
+else:
+    xp = fh * fr
+print("Pay:",xp)
