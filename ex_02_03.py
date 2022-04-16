a81782c8138ad9d4eea9ed97ed8b2f75dbf20a61
Tanya <thetwtanya@gmail.com>
2022-04-16 13:11:46 +0800

    ex_03_01.py

--- ex_02_03.py
+++ ex_02_03.py
@@ -2,3 +2,16 @@ xh = input("Enter Hours: ")
 xr = input("Enter Rate: ")
 xp = float(xh) * float(xr)
 print("Pay:",xp)
+
+
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter Rate:")
+r = float(rate)
+gp = float(h * r)
+if h <= 40:
+    gp = float(h * r)
+else:
+    gp = float(40 * r + (h-40) * (1.5 * r))
+
+print(gp)
